535efa2ac6ce99fde01cfc6543a5c49190363932
EmadKhreis <emad.khreis@gmail.com>
2020-04-06 20:23:37 -0700

    Carousel JS
    Added a play/pause button to the carousel, and a JS for the interval and functions for the Midterm!

--- Midterm/script.js
+++ Midterm/script.js
@@ -1,9 +1,14 @@
 $(document).ready(function(){
 	//we write everything here
+    $('#productsCarousel').carousel({
+        interval: 3000
+    });
 
-	$('#productsCarousel').carousel({
-		interval: 5
-	})
-
+$('#playButton').click(function () {
+    $('#productsCarousel').carousel('cycle');
+});
+$('#pauseButton').click(function () {
+    $('#productsCarousel').carousel('pause');
+});
 
 });
