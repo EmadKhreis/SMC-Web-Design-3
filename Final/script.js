076d0747e6cbdd35a6c039e9cde1c69be8e0196d
EmadKhreis <emad.khreis@gmail.com>
2020-06-09 12:40:04 -0700

    Final Template
    laying the ground work to start coding the Final project!

---
+++ Final/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	//we write everything here
+    $('#productsCarousel').carousel({
+        interval: 3000
+    });
+
+$('#playButton').click(function () {
+    $('#productsCarousel').carousel('cycle');
+});
+$('#pauseButton').click(function () {
+    $('#productsCarousel').carousel('pause');
+});
+
+$("#modalSubscriptionForm").modal('show');
+
+});
